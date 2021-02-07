9ac3217bdab623d2d688ccb2f6b6c0ea3201f6a2
mizphses <info@gentoo.app>
2021-02-08 08:47:35 +0900

    Initial and v1

---
+++ lang/ja/theme_boost.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_cdmdljp
+ * @copyright 2021 Fuminori Ogawa based on Boost by 2016 Ryan Wyllie
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['advancedsettings'] = 'くわしい設定（せってい）';
+$string['backgroundimage'] = '背景の設定';
+$string['backgroundimage_desc'] = 'ここで選ばれた画像は背景になります。アップロードするとテーマをそのまま置きかえます。';
+$string['brandcolor'] = 'テーマカラー';
+$string['brandcolor_desc'] = 'アクセントとなる色です';
+$string['bootswatch'] = 'Bootswatch';
+$string['bootswatch_desc'] = 'BootswatchはBootstrapの色テーマ集です。';
+$string['choosereadme'] = 'CdmdljpはBoostをベースにした、モダンでなテーマです。このテーマは、Bootstrap 4を利用して新しいテーマを作成する際に、直接、または親テーマとして使用することを意図しています。';
+$string['currentinparentheses'] = '(現在の設定)';
+$string['configtitle'] = 'Cdmdljp';
+$string['generalsettings'] = '一般設定';
+$string['nobootswatch'] = 'なし';
+$string['pluginname'] = 'Cdmdljp';
+$string['presetfiles'] = '追加のプリセットファイル';
+$string['presetfiles_desc'] = 'プリセットファイルは、テーマの外観を劇的に変えるために使用することができます。自分でプリセットファイルを作成して共有する方法については <a href="https://docs.moodle.org/dev/boost_Presets">Cdmdljpプリセット</a> を、他の人が共有したプリセットについては <a href="https://archive.moodle.net/boost">プリセットリポジトリ</a> を参照してください。';
+$string['preset'] = 'テーマプリセット';
+$string['preset_desc'] = 'プリセットを選んで、テーマの見た目を大きく変えることができます。';
+$string['privacy:metadata'] = 'Cdmdljpテーマはいかなる個人情報も取得し、送信することはありません。';
+$string['rawscss'] = '生のSCSS';
+$string['rawscss_desc'] = 'このフィールドを使用して、スタイルシートの最後に注入されるSCSSまたはCSSコードを指定します。';
+$string['rawscsspre'] = '生の初期SCSS';
+$string['rawscsspre_desc'] = 'このフィールドでは、SCSSコードの初期化を指定できます。ほとんどの場合、変数を定義するためにこの設定を使用します。';
+$string['region-side-pre'] = '右';
+$string['privacy:metadata:preference:draweropennav'] = 'ドロワーメニューのナビゲーションを隠すか表示するかのユーザーの設定';
+$string['privacy:drawernavclosed'] = 'ナビゲーション・ドロワーは閉じています。';
+$string['privacy:drawernavopen'] = 'ナビゲーションドロワーは開いています。';
