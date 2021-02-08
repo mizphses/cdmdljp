de9219f17d18cc472a9c95aa0f2ddfc1f7c557e6
mizphses <info@gentoo.app>
2021-02-08 10:04:12 +0900

    Updater Shell

--- updater.sh
+++ updater.sh
@@ -2,4 +2,4 @@
 git init
 git remote set-url origin https://github.com/mizphses/cdmdljp.git
 git remote add origin https://github.com/mizphses/cdmdljp.git
-git pull
+git pull --set-upstream origin main
